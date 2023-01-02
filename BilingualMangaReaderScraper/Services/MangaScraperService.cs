06095c0e3f711b311623673be7526e02f8cad259
jcancellier <sonicjcan@hotmail.com>
2023-01-02 12:05:49 -0800

    add files

---
+++ BilingualMangaReaderScraper/Services/MangaScraperService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Xml;
+using HtmlAgilityPack;
+
+namespace BilingualMangaReaderScraper.Services
+{
+	public class MangaScraperService
+	{
+		public MangaScraperService()
+		{
+            // Load web page
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("https://bilingualmanga.net");
+
+            // Get book ids
+
+        }
+	}
+}
+
